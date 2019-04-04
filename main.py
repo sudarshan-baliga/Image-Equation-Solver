fb24fdd5539d4754ca848a27fe0b931457f078ad
sudarshanbaliga <baliga108@gmail.com>
2019-04-04 20:32:02 +0530

    messed up

--- main.py
+++ main.py
@@ -5,69 +5,62 @@ import pickle
 import imutils
 
 
+
 def preprocessRegion(img, y1, y2, x1, x2):
     x = img[y1:y2, x1: x2]
     x = imutils.resize(x, width=50, height=50)
     return x
 
 def slidingWindow(img):
-    width, height = img.shape
-    count = 3
+    height, width = img.shape
+    print(width)
+    img2 = img.copy()
     regions = []
     x1, x2 = 30, 250
-    while(count):
+    while(x2 <= width):
+        img2 = img.copy()
         regions.append(preprocessRegion(img ,0,220,x1, x2))
+        cv2.rectangle(img2, (x1, 0), (x2, 220), (255,0,0), 2)
+        cv2.imshow("sliding window", img2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         x1 += 200
         x2 += 200
-        count -= 1
     return regions
 
 
-img = cv2.imread('cropped.png')
+img = cv2.imread('handwritten.png')
+
+plt.imshow(img)
+plt.title("original image")
+plt.show()
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 # thresholding
-ret, img = cv2.threshold(gray,80,255,cv2.THRESH_BINARY_INV)
+ret, img = cv2.threshold(gray,100,255,cv2.THRESH_BINARY_INV)
+plt.imshow(img, cmap = 'gray')
+plt.title("segmented image")
+plt.show()
 
 # dilate the image
 kernel = np.ones((5, 5), np.uint8)
 img = cv2.dilate(img, kernel, iterations=10)
+plt.imshow(img, cmap = 'gray')
+plt.title("dilated image")
+plt.show()
 
 print("loading classifier")
-knnPickel = pickle.load(open("./knn.classifier", "rb"))
+knnPickel = pickle.load(open("./knn2.classifier", "rb"))
 knn = knnPickel["knn"]
 
-
+# get the regions with text
 regions = slidingWindow(img)
+result = []
 for reg in regions:
-    plt.imshow(reg)
+    reg = (reg < 50).astype(int)
+    sym = knn.predict(reg.reshape(1,-1))
+    plt.imshow(reg, cmap='gray')
+    plt.title("region with symbol " + sym[0])
     plt.show()
-    reg = (reg < 100).astype(int)
-    print(reg.shape)
-    print(knn.predict(reg.reshape(1,-1)))
-
-    # cv2.imshow("eight", region)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-# plus = preprocessRegion(img, 120, 340, 450, 670)
-
-
-# three = preprocessRegion(img, 120, 340, 600, 820)
-# plt.imshow(three)
-# plt.show()
-
-
-# img1
-# # make it binary
-# one = (one < 100).astype(int)
-# plus = (plus < 100).astype(int)
-# three = (three < 100).astype(int)
-# eight = (eight < 100).astype(int)
-
-# print("loading classifier")
-# knnPickel = pickle.load(open("./knn.classifier", "rb"))
-# knn = knnPickel["knn"]
-# print(knn.predict(one.reshape(1,-1)), knn.predict(plus.reshape(1,-1)), knn.predict(two.reshape(1,-1)), )
-# print(knn.predict(eight.reshape(1,-1)), knn.predict(plus.reshape(1,-1)), knn.predict(three.reshape(1,-1)))
-
-# print("Accuracy:",metrics.accuracy_score(testLabels, testRes))
+    result.append(sym[0])
+print(result)
